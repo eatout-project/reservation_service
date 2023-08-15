d3eda10fb393e4dd632ffbffccc61f4bd79ca236
chma <christian.em@live.dk>
2023-08-15 10:47:10 +0200

    updated everything for production and development mode

--- controllers/reservation.ts
+++ controllers/reservation.ts
@@ -25,14 +25,19 @@ export const createReservationRequest = (req: Request, res: Response, db: Knex) 
 }
 
 export const getCustomerReservations = (req: Request, res: Response, db: Knex) => {
-    db.select('*').from('reservations').where('customerId', req.body.id)
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(400).json('Could not register reservation')
-        });
+    db.transaction(trx => {
+        trx.select('*').from('reservations').where('customerId', req.body.id)
+            .then(data => {
+                console.log('found reservations: ', data);
+                trx.commit();
+                res.status(200).json(data);
+            })
+            .catch(error => {
+                console.log(error);
+                trx.rollback();
+                res.status(400).json('Could not register reservation')
+            });
+    })
 }
 
 export const updateReservationRequest = (req: Request, res: Response, db: Knex) => {
@@ -53,23 +58,31 @@ export const updateReservationRequest = (req: Request, res: Response, db: Knex) 
 export const getRestaurantAcceptedReservations = (req: Request, res: Response, db: Knex) => {
     console.log('in here')
     console.log('body: ', req.body);
-    db.select('*').from('reservations').where('restaurantId', req.body.id).andWhere('status', 'approved')
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(400).json('Could not find any accepted reservations')
-        });
+    db.transaction(trx => {
+        trx.select('*').from('reservations').where('restaurantId', req.body.id).andWhere('status', 'approved')
+            .then(data => {
+                trx.commit();
+                res.status(200).json(data);
+            })
+            .catch(error => {
+                console.log(error);
+                trx.rollback();
+                res.status(400).json('Could not find any accepted reservations')
+            });
+    })
 }
 
 export const getRestaurantWaitingReservations = (req: Request, res: Response, db: Knex) => {
-    db.select('*').from('reservations').where('restaurantId', req.body.id).andWhere('status', 'waiting')
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(400).json('Could not register reservation')
-        });
+    db.transaction(trx => {
+      trx.select('*').from('reservations').where('restaurantId', req.body.id).andWhere('status', 'waiting')
+          .then(data => {
+              trx.commit();
+              res.status(200).json(data);
+          })
+          .catch(error => {
+              console.log(error);
+              trx.rollback();
+              res.status(400).json('Could not register reservation')
+          });
+    })
 }
--- index.ts
+++ index.ts
@@ -31,12 +31,16 @@ const db = knex({
 
 console.log(
     {
-        host: process.env.CUSTOMER_LOGIN_DB_HOST ? `${process.env.CUSTOMER_LOGIN_DB_HOST}` : `127.0.0.1`,
-        port: process.env.CUSTOMER_LOGIN_DB_PORT ? parseInt(`${process.env.CUSTOMER_LOGIN_DB_PORT}`) : 3308,
-        user: process.env.CUSTOMER_LOGIN_DB_USER ? `${process.env.CUSTOMER_LOGIN_DB_USER}` : `root`,
-        database: process.env.CUSTOMER_LOGIN_DB ? `${process.env.CUSTOMER_LOGIN_DB}` : 'db'
+        host: process.env.RESERVATION_DB_HOST ? `${process.env.RESERVATION_DB_HOST}` : `127.0.0.1`,
+        port: process.env.RESERVATION_DB_PORT ? parseInt(`${process.env.RESERVATION_DB_PORT}`) : 3308,
+        user: process.env.RESERVATION_DB_USER ? `${process.env.RESERVATION_DB_USER}` : `root`,
+        database: process.env.RESERVATION_DB ? `${process.env.RESERVATION_DB}` : 'db'
     }
 )
+
+app.get('/',  (req: Request, res: Response) => {
+    return res.status(200).json('Reservation service is running')
+});
 
 app.post('/createReservationRequest',  (req: Request, res: Response) => createReservationRequest(req, res, db));
 
